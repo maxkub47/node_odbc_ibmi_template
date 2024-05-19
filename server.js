0dbcb7c7ff639f8609252e7f3aa66b822ef2e680
Rattapon Liganporn <maxkub47@gmail.com>
2024-05-19 18:41:21 +0700

    add api-doc

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const { json } = require("body-parser");
 const morgan = require("morgan");
 const cors = require("cors");
-
 const session = require('express-session')
 
 const jwt = require('./SRC/helpers/jwt');
@@ -10,6 +9,13 @@ const errorHandler = require('./SRC/helpers/error-handler');
 const { Database } = require('./SRC/helpers/odbc')
 const { connectionString } = require('./SRC/helpers/odbc')
 const { login } = require('./SRC/helpers/login')
+
+//swagger Document
+const swaggerUi = require('swagger-ui-express');
+const fs = require("fs")
+const YAML = require('yaml')
+const file  = fs.readFileSync('./swagger.yaml', 'utf8')
+const swaggerDocument = YAML.parse(file)
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -20,6 +26,7 @@ app.use(cors());
 app.use(morgan("dev"));
 //app.use(jwt())
 app.use(session({secret: process.env.SES_SECRET, resave: true, saveUninitialized: true}))
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 //Routes
 app.get('/', (req, res) => {
